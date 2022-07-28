e32d71c5803649355f4821d46fa87adf1788e30d
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-28 23:20:54 +0530

    Add files via upload

---
+++ 1/super.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import  pandas as pd
+
+dataset = pd.read_csv("Data.csv")
+
+matrix = dataset.iloc[:, :-1].values
+
+arr = dataset.iloc[:,3].values
