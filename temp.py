7d024813639400b27d3692b23310e327ad34532a
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-29 23:22:47 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd
+from sklearn.impute import SimpleImputer as Imputer
+dataset = pd.read_csv("C:/Users/microsoft/Desktop/#/python programs/ML/1/Data.csv")
+
+x = dataset.iloc[:, :-1]
+
+y = dataset.iloc[:, 3]
+narayan ="narayan narayan narayan narayan"
+print(narayan)
+
+
+imputer = Imputer
