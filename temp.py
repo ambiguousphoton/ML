78d6fbc53a08598bcf97ad8ba6da5c270b3fb8e5
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-31 22:59:04 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,11 +4,12 @@ import pandas as pd
 from sklearn.impute import SimpleImputer as Imputer
 dataset = pd.read_csv("C:/Users/microsoft/Desktop/#/python programs/ML/1/Data.csv")
 
-x = dataset.iloc[:, :-1]
+x = dataset.iloc[:, :-1].value
 
-y = dataset.iloc[:, 3]
+y = dataset.iloc[:, 3].value
 narayan ="narayan narayan narayan narayan"
 print(narayan)
 
 
-imputer = Imputer
+imputer = Imputer.fit(x[:,1:])
+imputer = imputer.fit(x[:, 1:3])
