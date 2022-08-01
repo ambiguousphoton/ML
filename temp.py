2c1484f137aff35d4312ae1d57a8948c7123786f
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-01 19:59:22 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 from sklearn.impute import SimpleImputer as Imputer
-dataset = pd.read_csv("C:/Users/microsoft/Desktop/#/python programs/ML/1/Data.csv")
+from sklearn.preprocessing import LabelEncoder , OneHotEncoder
+dataset = pd.read_csv("Data.csv")
 
 x = dataset.iloc[:, :-1].value
 
@@ -11,5 +12,15 @@ narayan ="narayan narayan narayan narayan"
 print(narayan)
 
 
-imputer = Imputer.fit(x[:,1:])
-imputer = imputer.fit(x[:, 1:3])
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean' , fill_value=None, verbose="deprecated", copy=True, add_indicator=False)
+imputer = imputer.fit(x[:, 1:]) 
+x[:, 1:3] =  imputer.transform(x[:, 1:3])  
+labelEncoder_x = LabelEncoder()
+x[:, 0] = labelEncoder_x.fit_transform(x[:,0])
+onehotencoder = OneHotEncoder(categories = [0])
+x =onehotencoder.fit_transform(x[0,:]).toarray()
+
+
+
+
+
