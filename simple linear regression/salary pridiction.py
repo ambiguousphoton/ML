fd3d8bd10eac55b706b758cd9efcf27db5e76745
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-04 23:00:43 +0530

    Add files via upload

---
+++ simple linear regression/salary pridiction.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+test = "narayan narayan narayan narayan"
+dataset = pd.read_csv('P12-SalaryData.csv')
+
+X = dataset.iloc(:,:-1)
+y = dataset.iloc(:,1)
+print(test)
