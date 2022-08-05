c7b22195a034a4d88c577cb906b7b3135b8c90e9
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-05 23:48:30 +0530

    Add files via upload

---
+++ multiple regression dataset/multiple linear regression.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+narayan = "narayan "
+n4 = narayan +narayan+narayan+narayan
+dataset = pd.read_csv("startups.csv")
+
+
+print(n4)
