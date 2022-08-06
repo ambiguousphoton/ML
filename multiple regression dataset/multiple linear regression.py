408b9385e55418933283b010e1ef73d8a406257a
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-06 23:25:25 +0530

    Add files via upload

--- multiple regression dataset/multiple linear regression.py
+++ multiple regression dataset/multiple linear regression.py
@@ -1,9 +1,23 @@
-import numpy as np 
-import pandas as pd
-import matplotlib.pyplot as plt
-narayan = "narayan "
-n4 = narayan +narayan+narayan+narayan
+# Multiple linear regression
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# Import dataset
 dataset = pd.read_csv("startups.csv")
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,4].values
+
+# Splitting the dataset into training set and test dataset 
+from sklearn.model_selection import train_test_split 
+X_train , X_test , y_train , y_test = train_test_split(x, y ,test_size=0.25) 
 
 
-print(n4)
+# Creating the dummy variables 
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+x[:,3] = LabelEncoder().fit_transform(x[:,3])
+x = OneHotEncoder(categories=[3]).fit_transform(x).toarray() 
+
+
+# Feature Scaling 
+
