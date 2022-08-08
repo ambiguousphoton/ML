4ad57734eb7bc96d1d658f9fbe6aec8d989d14ba
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-08 17:26:07 +0530

    Add files via upload

---
+++ Decision Tree Intuition/Decision Tree.py
@@ -0,0 +1,23 @@
+### non continues non linear model
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 2:3].values
+y = dataset.iloc[:, 3].values
+
+# Fitting the decision tree regressor to dataset 
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(x,y)
+
+y_prd = regressor.predict([[6.5]])
+
+# visualising tree regressor
+x_grid = np.arange(min(x), max(x), 0.01)
+x_grid = x_grid.reshape((len(x_grid),1))
+plt.scatter(x, y , color='red')
+plt.plot(x_grid,regressor.predict(x_grid),color='blue')
+plt.title('Decision Tree Regression')
+plt.show()
