d14b68fa84df0dd029d334d4af5c924f2623039f
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-09 11:18:42 +0530

    Add files via upload

---
+++ K - Nearest Neighbours/K NEAREST NEIGHBOURS.py
@@ -0,0 +1,66 @@
+# K NEAREST NEIGHBOURS
+import numpy as np
+import matplotlib.pyplot  as plt 
+import pandas as pd
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,4].values
+
+# SPLITTING THE TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
+X_train , X_test , y_train , y_test = train_test_split(x , y , test_size=0.25, random_state=0 ) 
+
+# FEATURE SCALING
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+#FITTING CLASSIFIER TO TRAINING SET
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5,metric='minkowski', p = 2)
+classifier.fit(X_train, y_train)
+
+# PREDICT THE TEST SET RESULTS
+y_prediction = classifier.predict(X_test)
+
+# MAKING THE CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_prediction)
+
+# VISUALATION OF TRAININGSET PREDICTION
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN Regression (Train set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# VISUALISATION OF TESTSET PREDICTION
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
