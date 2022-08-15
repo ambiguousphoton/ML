4c64abb12282983dad1b49970b1223fef5587509
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-15 19:06:59 +0530

    Add files via upload

---
+++ KEPLER RESEARCH DATA/Decision tree sun's radius prediction.py
@@ -0,0 +1,47 @@
+### non continues non linear model
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('C:/Users/microsoft/Desktop/#/python programs/ML/OM/LINEAR REGRESSION/dataset.csv')
+x = dataset.iloc[:, [26]].values
+y = dataset.iloc[:, [49]].values
+
+
+from sklearn.impute import SimpleImputer 
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean' , fill_value=None, verbose="deprecated", copy=True, add_indicator=False)# imputer objecet
+imputer = imputer.fit(y[:, :]) 
+y[:, :] = imputer.transform(y[:, :]) 
+
+from sklearn.impute import SimpleImputer 
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean' , fill_value=None, verbose="deprecated", copy=True, add_indicator=False)# imputer objecet
+imputer = imputer.fit(x[:, :]) 
+x[:, :] = imputer.transform(x[:, :]) 
+
+
+from sklearn.model_selection import train_test_split
+X_train , X_test , y_train , y_test = train_test_split(x,y,train_size=0.25)  
+
+
+# Fitting the decision tree regressor to dataset 
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X_train,y_train)
+
+# visualising tree regressor
+x_grid = np.arange(min(x), max(x), 0.01)
+x_grid = x_grid.reshape((len(x_grid),1))
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(x_grid, regressor.predict(x_grid), color='blue')
+plt.title('Decission Prediction (train)')
+plt.ylabel("PLAENTS RADIUS")
+plt.xlabel("SUNS RADIUS")
+plt.show()
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(x_grid, regressor.predict(x_grid), color='blue')
+plt.title('Decission Prediction (test)')
+plt.ylabel("PLANETS RADIUS")
+plt.xlabel("SUNS RADIUS")
+plt.show()
