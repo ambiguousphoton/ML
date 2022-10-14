c46ed57fc1187261508cfb8bf46f624e8c292068
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-10-14 23:29:32 +0530

    Update NAIVE BAYES CLASSIFIER.py

--- Naive Bayes/NAIVE BAYES CLASSIFIER.py
+++ Naive Bayes/NAIVE BAYES CLASSIFIER.py
@@ -6,7 +6,8 @@ import pandas as pd
 dataset = pd.read_csv('Social_Network_Ads.csv')
 x = dataset.iloc[:,[2,3]].values
 y = dataset.iloc[:,4].values
-
+Id = 1
+Name ="Vyoam"
 # SPLITTING THE TRAINING SET AND TEST SET
 from sklearn.model_selection import train_test_split
 X_train , X_test , y_train , y_test = train_test_split(x , y , test_size=0.25, random_state=0 ) 
