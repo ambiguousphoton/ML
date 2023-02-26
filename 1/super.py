cb7705439476a08a0c6a305361e61ac22469e39e
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2023-02-26 23:49:54 +0530

    Update super.py

--- 1/super.py
+++ 1/super.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import  pandas as pd
-By = "Vyoam Yadav ji"
+By = "Vyoam Yadav"
 dataset = pd.read_csv("Data.csv")
 
 matrix = dataset.iloc[:, :-1].values
